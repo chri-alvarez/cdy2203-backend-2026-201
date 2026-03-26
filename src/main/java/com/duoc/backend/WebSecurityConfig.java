146ad18d539b8c373324b6ee722f632050aae16f
CAL <cal@uc.cl>
2026-03-26 20:18:58 -0300

    Mascotas

--- src/main/java/com/duoc/backend/WebSecurityConfig.java
+++ src/main/java/com/duoc/backend/WebSecurityConfig.java
@@ -27,6 +27,7 @@ class WebSecurityConfig{
                 .authorizeHttpRequests( authz -> authz
                         .requestMatchers(HttpMethod.POST, Constants.LOGIN_URL).permitAll()
                         .requestMatchers(HttpMethod.GET, Constants.LOGIN_URL).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/pets/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
